f0b29a83a8ce679b59826fe5d01bcd2cdd42333a
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-26 13:57:26 +0300

    arrays of objects

--- src/015-essential-types-and-annotations/029-arrays-of-objects.problem.ts
+++ src/015-essential-types-and-annotations/029-arrays-of-objects.problem.ts
@@ -1,6 +1,12 @@
 type Recipe = {
   title: string;
+  ingredients: Ingredients[];
   instructions: string;
+};
+
+type Ingredients = {
+  name: string;
+  quantity: string;
 };
 
 const processRecipe = (recipe: Recipe) => {
