a09028fca9442bc38669b7e39d946fd5ed02b58d
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-26 14:01:05 +0300

    tuples

--- src/015-essential-types-and-annotations/031-tuples.problem.ts
+++ src/015-essential-types-and-annotations/031-tuples.problem.ts
@@ -1,6 +1,6 @@
 import { Expect, Equal } from "@total-typescript/helpers";
 
-const setRange = (range: Array<number>) => {
+const setRange = (range: [x: number, y: number]) => {
   const x = range[0];
   const y = range[1];
 
