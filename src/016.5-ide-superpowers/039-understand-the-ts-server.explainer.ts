190aff70428199b574fafe74b3e3f0eb67cf1c5c
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-27 15:51:56 +0300

    ts server

--- src/016.5-ide-superpowers/039-understand-the-ts-server.explainer.ts
+++ src/016.5-ide-superpowers/039-understand-the-ts-server.explainer.ts
@@ -1,1 +1,8 @@
 // Explain Diagram
+/* 
+You open VSCode → TS Server starts
+You change a file → TS Server checks your code
+Red line appears → TS Server finds an error
+You fix the error → No more error remains
+Red line disappears → You close VSCode → TS Server stops 
+*/
