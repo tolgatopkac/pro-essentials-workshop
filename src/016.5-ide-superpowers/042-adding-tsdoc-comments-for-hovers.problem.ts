b77157de26e8e1ae202c8f1a5e74484280767c3d
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-27 15:56:42 +0300

    tsdoc comments for hovers

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,14 @@
+/**
+ * Adds two numbers together.
+ *
+ * @example
+ *
+ * myFunction(1,2);
+ * // Tutorial
+ * */
+
 const myFunction = (a: number, b: number) => {
   return a + b;
 };
+
+myFunction(1, 2);
